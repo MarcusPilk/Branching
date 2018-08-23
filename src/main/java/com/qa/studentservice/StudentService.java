630c84558e9f658371ca019ff77133122c3db7ff
MarcusPilk <marcuspilkington@outlook.com>
2018-08-23 10:00:34 +0100

    Student class and service created

---
+++ src/main/java/com/qa/studentservice/StudentService.java
@@ -0,0 +1,19 @@
+package com.qa.studentservice;
+
+import com.qa.students.Student;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StudentService {
+    private Map<Integer,Student> studentMap;
+    private static int counter = 1;
+
+    public StudentService() {
+        studentMap = new HashMap<Integer, Student>();
+    }
+    public void addStudentToMap(Student studentToAdd){
+        studentMap.put(counter,studentToAdd);
+        counter++;
+    }
+}
