a02dd116af58dcf315ab7187fc82d2ff504f2a08
MarcusPilk <marcuspilkington@outlook.com>
2018-08-23 14:06:13 +0100

    SonarQube smells amended

--- src/main/java/com/qa/app/App.java
+++ src/main/java/com/qa/app/App.java
@@ -5,20 +5,20 @@ import com.qa.students.Student;
 
 
 public class App {
+    private StudentService studentService;
     private App() {
-        StudentService studentService = new StudentService();
+        studentService = new StudentService();
 
         studentService.addStudentToMap(new Student("Marcus","Pilkington",1234));
         studentService.addStudentToMap(new Student("Test","Student2",1234));
-
-        for (int entry : studentService.getStudentMap().keySet()) {
-            System.out.println(studentService.getStudentMap().get(entry).toString());
-        }
     }
 
 
 
     public static void main(String[] args) {
-        new App();
+        App app = new App();
+        for (int entry : app.studentService.getStudentMap().keySet()) {
+            System.out.println(app.studentService.getStudentMap().get(entry).toString());
+        }
     }
 }
